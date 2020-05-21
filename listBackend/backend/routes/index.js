54bae7eba5dd7da912ef4b452468e34e5f83f56d
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-05-21 13:54:11 +0200

    Refine the content of email

--- listBackend/backend/routes/index.js
+++ listBackend/backend/routes/index.js
@@ -22,28 +22,36 @@ transporter.verify((error, success) => {
   }
 });
 
-router.post('/send', (req, res, next) => {
-  const fruits = req.body.fruits
-  const vegetables = req.body.vegetables
-  const dairy = req.body.dairy
-  const meatAndFish = req.body.meatAndFish
-  const dryGoods = req.body.dryGoods
-  const householdItems = req.body.householdItems
-  const others = req.body.others
-  const email = req.body.email
-  const message = req.body.message
+router.post('/send', (req, res) => {
 
-  const content = `
-  SHOPPING LIST:
-  Fruits: ${fruits}
-  Vegetables: ${vegetables}
-  Dairy: ${dairy}
-  Meat and fish: ${meatAndFish}
-  Dry goods: ${dryGoods}
-  Household items: ${householdItems}
-  Others: ${others}
-  Message: ${message}
-  `
+  const reg = new RegExp(/[A-Z]*\:\s$/)
+
+  const fruits = reg.test(req.body.fruits.toString()) ? null : req.body.fruits;
+  const vegetables = reg.test(req.body.vegetables.toString()) ? null : req.body.vegetables;
+  const dairy = reg.test(req.body.dairy.toString()) ? null : req.body.dairy;
+  const meatAndFish = reg.test(req.body.meatAndFish.toString()) ? null : req.body.meatAndFish;
+  const dryGoods = reg.test(req.body.dryGoods.toString()) ? null : req.body.dryGoods;
+  const householdItems = reg.test(req.body.householdItems.toString()) ? null : req.body.householdItems;
+  const others = reg.test(req.body.others.toString()) ? null : req.body.others;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  function isNotNull(value) {
+    return value !== null;
+  }
+
+  const content = [
+    'SHOPPING LIST:',
+    fruits,
+    vegetables,
+    dairy,
+    meatAndFish,
+    dryGoods,
+    householdItems,
+    others,
+    '\r\n',
+    message].filter(isNotNull)
+    .join("\r\n").toString()
 
   const mail = {
     from: 'shoppinglist55555@gmail.com',
