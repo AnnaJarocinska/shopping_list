0eea556a01ea3d6426f0184aa5b6b8a088733fad
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-06-09 10:37:39 +0200

    Implement i18next and add Test in SendingList view

--- list/src/app/views/SendingList.js
+++ list/src/app/views/SendingList.js
@@ -1,37 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
+import '../../i18n'
+
 import SendingEmail from '../email/components/SendingEmail';
+import Test from '../components/translation/Test';
+import LanguageSelector from '../components/translation/LanguageSelector';
+
+
 
 const SendingList = (props) => {
 
-    const shoppingList = [];
-    for (let [key, value] of Object.entries(props.products)){
-        if (props.products[key].list.length !== 0) {
-        shoppingList.push(
-            <div 
-            key={key}>
-                <p>{value.name}</p>
-                <ul>{value.list.map((product) =>
-                    <li
-                        id={value.list.indexOf(product)}
-                        key={product}
-                        name={product}
-                        >
-                        {product}
-                    </li>)}
-                </ul>
-            </div>)
-    }
-}
+   
+//     const shoppingList = [];
+//     for (let [key, value] of Object.entries(props.products)){
+//         if (props.products[key].list.length !== 0) {
+//         shoppingList.push(
+//             <div 
+//             key={key}>
+//                 <p>{value.name}</p>
+//                 <ul>{value.list.map((product) =>
+//                     <li
+//                         id={value.list.indexOf(product)}
+//                         key={product}
+//                         name={product}
+//                         >
+//                         {product}
+//                     </li>)}
+//                 </ul>
+//             </div>)
+//     }
+// }
 
     return (<Fragment>
-  <p>Sending list</p>
-  {shoppingList}
-
-           <SendingEmail/>
-           <Link to="/">Back to adding recipent</Link>
-        </Fragment>
+                <Suspense fallback={null}>
+                {/* <p>Sending list</p> */}
+                {/* {shoppingList} */}
+                    <LanguageSelector/>
+                    <Test/>
+                </Suspense>
+                 <SendingEmail/>
+                <Link to="/">Back to adding recipent</Link>
+            </Fragment>
     );
 }
 
--- list/src/index.js
+++ list/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import store from './store';
+import './i18n';
+import {I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
+
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+});
 
 ReactDOM.render(
-<Provider store = {store}>
-<App />
-</Provider>
-, document.getElementById('root'));
+    <Provider store={store}>
+         <I18nextProvider i18n={i18next}>
+        <App />
+        </I18nextProvider>,
+    </Provider>
+    , document.getElementById('root'));
 
 serviceWorker.unregister();
