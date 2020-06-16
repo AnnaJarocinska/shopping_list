15fa8b044e74cbd7011e0f8d6e731f639352807d
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-06-16 23:20:41 +0200

    Delete others file

--- list/src/app/others/duck/index.js
+++
@@ -1,5 +0,0 @@
-import othersReducer from './reducers';
-export {default as othersTypes} from './types';
-export {default as othersActions} from './actions';
-
-export default othersReducer;
--- list/src/app/others/duck/types.js
+++
@@ -1,7 +0,0 @@
-const ADD_OTHER = 'ADD_OTHER';
-const RESET_OTHERS = 'RESET_OTHERS';
-
-export default {
-ADD_OTHER,
-RESET_OTHERS,
-}
