18e4682179a57a73f4610d7185ce73cec619bd3d
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-07-16 22:23:20 +0200

    Make build

---
+++ list/build/precache-manifest.28d25d0d13038a08806b53f574b7fb82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe3bc86e1a97657f535d6b9fe929ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "628c8a3cc253253fabc3",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "628c8a3cc253253fabc3",
+    "url": "/static/js/2.985e1569.chunk.js"
+  },
+  {
+    "revision": "853265c4b299247554809ca833e64944",
+    "url": "/static/js/2.985e1569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669bfa0b32a514c20882",
+    "url": "/static/js/main.cb595db9.chunk.js"
+  },
+  {
+    "revision": "8618a7eaaf24f3e4897d",
+    "url": "/static/js/runtime-main.8fa136a4.js"
+  },
+  {
+    "revision": "a9113b02f9bd946a612cd3571daef6b4",
+    "url": "/static/media/bananas_S.a9113b02.png"
+  },
+  {
+    "revision": "a3666e573f52af75ff97b5ed4d019ccd",
+    "url": "/static/media/basket_S.a3666e57.png"
+  },
+  {
+    "revision": "b57dd0ea2106b4cef96d52e80490fd01",
+    "url": "/static/media/cherry_S.b57dd0ea.png"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "f2180a83f853056a77be05080a74fe5a",
+    "url": "/static/media/milkyWay_L.f2180a83.jpg"
+  },
+  {
+    "revision": "eedcc37c5e5365b2bc335ca3f8496d61",
+    "url": "/static/media/salad_S.eedcc37c.png"
+  }
+]);
