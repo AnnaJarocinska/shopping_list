769d7ffb6a0e301806f918df6322ed3938abdd27
Anna Jarocińska <anna.jarocinska5@gmail.com>
2020-07-17 22:09:08 +0200

    Add start-frontend and start-backend to package.json

--- listBackend/backend/app.js
+++ listBackend/backend/app.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/', indexRouter);
 
